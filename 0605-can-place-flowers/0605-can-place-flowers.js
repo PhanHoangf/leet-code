ead5f99d4b0f78b4fc7864e70bfa98c69ccd226c
Hazel <44758067+PhanHoangf@users.noreply.github.com>
2024-07-04 11:09:54 +0700

    Time: 60 ms (50.32%), Space: 51 MB (67.76%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -1,39 +1,9 @@
 var canPlaceFlowers = function (flowerbed, n) {
     if (n === 0) return true;
     
-    // if (flowerbed[0] === 0) {
-    //     if (flowerbed.length === 1) return true;
-        
-    //     if (flowerbed[1] === 0) {
-    //         n -= 1;
-    //         flowerbed[0] = 1;
-    //     }
-    // }
-
-    // if (n === 0) return true;
-
-    // if (flowerbed[flowerbed.length - 1] === 0) {
-    //     if (flowerbed[flowerbed.length - 2] === 0) {
-    //         n -= 1;
-    //         flowerbed[flowerbed.length - 1] = 1;
-    //     }
-    // }
-
-    // if (n === 0) return true;
-
-
     for (let i = 0; i < flowerbed.length; i++) {
         let prev = flowerbed[i - 1] || 0;
         let aft = flowerbed[i + 1] || 0;
-
-        // console.log("//////////////////////////")
-        // console.log(prev, "<== prev")
-        // console.log(n, "<== loop")
-        // console.log(aft, "<== aft")
-        // console.log(i, "<== i")
-        // console.log("//////////////////////////")
-
-
 
         if (prev === 0 && aft === 0 && flowerbed[i] === 0) {
             n -= 1;
