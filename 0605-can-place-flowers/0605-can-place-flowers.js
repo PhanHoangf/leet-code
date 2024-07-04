eafd39f2d77c924c2ca6ccc004ed4250d3fd1fc3
Hazel <44758067+PhanHoangf@users.noreply.github.com>
2024-07-04 11:08:47 +0700

    Time: 45 ms (98.08%), Space: 50.3 MB (98.03%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -1,42 +1,37 @@
-/**
- * @param {number[]} flowerbed
- * @param {number} n
- * @return {boolean}
- */
 var canPlaceFlowers = function (flowerbed, n) {
     if (n === 0) return true;
-    if (flowerbed[0] === 0) {
-        if (flowerbed.length === 1) return true;
+    
+    // if (flowerbed[0] === 0) {
+    //     if (flowerbed.length === 1) return true;
         
-        if (flowerbed[1] === 0) {
-            n -= 1;
-            flowerbed[0] = 1;
-        }
-    }
+    //     if (flowerbed[1] === 0) {
+    //         n -= 1;
+    //         flowerbed[0] = 1;
+    //     }
+    // }
 
-    if (n === 0) return true;
+    // if (n === 0) return true;
 
-    if (flowerbed[flowerbed.length - 1] === 0) {
-        if (flowerbed[flowerbed.length - 2] === 0) {
-            n -= 1;
-            flowerbed[flowerbed.length - 1] = 1;
-        }
-    }
+    // if (flowerbed[flowerbed.length - 1] === 0) {
+    //     if (flowerbed[flowerbed.length - 2] === 0) {
+    //         n -= 1;
+    //         flowerbed[flowerbed.length - 1] = 1;
+    //     }
+    // }
 
-    if (n === 0) return true;
+    // if (n === 0) return true;
 
-    console.log(n)
 
-    for (let i = 1; i < flowerbed.length - 1; i++) {
-        let prev = flowerbed[i - 1];
-        let aft = flowerbed[i + 1];
+    for (let i = 0; i < flowerbed.length; i++) {
+        let prev = flowerbed[i - 1] || 0;
+        let aft = flowerbed[i + 1] || 0;
 
-        console.log("//////////////////////////")
-        console.log(prev, "<== prev")
-        console.log(n, "<== loop")
-        console.log(aft, "<== aft")
-        console.log(i, "<== i")
-        console.log("//////////////////////////")
+        // console.log("//////////////////////////")
+        // console.log(prev, "<== prev")
+        // console.log(n, "<== loop")
+        // console.log(aft, "<== aft")
+        // console.log(i, "<== i")
+        // console.log("//////////////////////////")
 
 
 
@@ -46,9 +41,6 @@ var canPlaceFlowers = function (flowerbed, n) {
             if (n === 0) break;
         }
     }
-
-    console.log(flowerbed)
-    console.log(n)
 
     return n === 0;
 };
