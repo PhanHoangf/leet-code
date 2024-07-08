ab3789ed5d40d0274182d6f982999f03ad800668
Hazel <44758067+PhanHoangf@users.noreply.github.com>
2024-07-08 14:57:10 +0700

    Time: 75 ms (12.56%), Space: 51.5 MB (71.49%) - LeetHub

--- 0443-string-compression/0443-string-compression.js
+++ 0443-string-compression/0443-string-compression.js
@@ -3,14 +3,11 @@
  * @return {number}
  */
 var compress = function (chars) {
-
     if (chars.length === 1) return chars.length;
 
     let i = 0;
     let j = i + 1;
     let count = 1;
-    let res = 0;
-    let previousLength = chars.length;
 
     while (j <= chars.length - 1) {
         if (chars[i] === chars[j]) {
