5fba4ca925a6a40226795f5a345b4d446ec2926d
Hazel <44758067+PhanHoangf@users.noreply.github.com>
2024-07-29 09:58:34 +0700

    Time: 243 ms (5.15%), Space: 59.9 MB (96.36%) - LeetHub

---
+++ 2470-removing-stars-from-a-string/2470-removing-stars-from-a-string.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function (s) {
+    let result = '';
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === '*') {
+            result = result.slice(0, -1);
+        } else result += s[i];
+    }
+
+    return result;
+};
