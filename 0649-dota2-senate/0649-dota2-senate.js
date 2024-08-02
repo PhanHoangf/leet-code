d807efaf5997b3166de2c7423744d40080689086
Hazel <44758067+PhanHoangf@users.noreply.github.com>
2024-08-02 13:56:36 +0700

    Time: 180 ms (8.05%), Space: 51.3 MB (86.41%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} senate
+ * @return {string}
+ */
+var predictPartyVictory = function (senate) {
+    senate = senate.split('');
+
+    while (senate.length !== 1) {
+        let teamName = senate.shift();
+
+        if (teamName !== '') {
+            for (let i = 0; i < senate.length; i++) {
+                if (senate[i] !== teamName && senate[i] !== '') {
+                    senate[i] = ''
+                    senate.push(teamName);
+                    break;
+                }
+            }
+        }
+    }
+
+    return senate[0] === 'R' ? "Radiant" : "Dire"
+};
