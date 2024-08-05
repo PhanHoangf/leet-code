8e82a9b8fc5ecd4cc33e99c8b58bea322e099f7f
Hazel <44758067+PhanHoangf@users.noreply.github.com>
2024-08-06 00:44:26 +0700

    Time: 68 ms (55.57%), Space: 52.8 MB (80.66%) - LeetHub

---
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var oddEvenList = function (head) {
+    if (head == null) return null;
+    if (head.next == null) return head;
+    let p = head;
+    let test = new ListNode(head.next.val);
+    let r = test;
+    while (p.next != null && p.next.next != null) {
+        p.next = p.next.next;
+        p = p.next;
+        r.next = p.next;
+        r = r.next;
+    }
+    p.next = test;
+    return head;
+};
